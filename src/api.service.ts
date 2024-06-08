20d0d9380b2d0c914e510c51271f678ce8d0d95f
nicolaes <nicolae.stihhi@gmail.com>
2024-06-08 19:35:43 +0300

    gcp proxy

--- src/api.service.ts
+++ src/api.service.ts
@@ -7,8 +7,8 @@ import { getUnixTime, isValid, parse } from "date-fns";
 
 const BASE_URL = atob('aHR0cHM6Ly9hcGl2Mi51cGZpdC5iaXo=');
 const TASKS_URL = 'https://europe-west3-seventh-magnet-307411.cloudfunctions.net/bookclass';
-const postBody = new HttpParams().set('project', 'wcr').set('lang', '2').toString();
-const postHeaders = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+const postBody = 'project=wcr';
+const postHeaders = {'Content-Type': 'application/x-www-form-urlencoded'}
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
@@ -16,18 +16,18 @@ export class ApiService {
 
     private _queue = '';
     private _token = '';
-    private _headers = postHeaders;
+    private _headers: {[key: string]: string} = postHeaders;
     public get token() { return this._token; }
 
     login(username: string, password: string): Observable<{ successful: boolean, error?: string }> {
         const creds = `email=${encodeURI(username)}&member_password=${encodeURI(password)}`;
         const url = `${BASE_URL}/authenticate.php?json&${creds}`;
-        return this.httpClient.post<LoginResult>(url, postBody, { headers: postHeaders }).pipe(
+        return this.postRequest<LoginResult>(url, `${postBody}&lang=2`, postHeaders).pipe(
             map((result) => {
                 if (result.token) {
                     this._queue = sha256.create().update('bookclass_' + username).digest().toHex();
                     this._token = result.token;
-                    this._headers = postHeaders.set('Auth', `Bearer ${this._token}`);
+                    this._headers = {...postHeaders, Auth: `Bearer ${this._token}`};
                     return { successful: true };
                 } else {
                     this._queue = '';
@@ -40,7 +40,7 @@ export class ApiService {
     }
 
     getClubs(): Observable<ClubDto[]> {
-        return this.httpClient.post<Clubs>(`${BASE_URL}/get-clubs.php?json`, postBody, { headers: this._headers })
+        return this.postRequest<Clubs>(`${BASE_URL}/get-clubs.php?json`, postBody, this._headers)
             .pipe(
                 map(clubs => {
                     const clubDtos: ClubDto[] = Object.values(clubs.clubs)
@@ -48,8 +48,8 @@ export class ApiService {
                             const name = c.short_name.replace(atob('V29ybGQgQ2xhc3Mg'), '');
                             const tier: string =
                                 Object.values(c.resources).filter(t => +t.tagid <= 5)?.[0]?.name || '';
-                            return { 
-                                id: c.clubid, 
+                            return {
+                                id: c.clubid,
                                 name: `${name} ${tier ? ' (' + tier.toLowerCase() + ')' : ''}`
                             };
                         })
@@ -69,7 +69,7 @@ export class ApiService {
             .set('date_end', days[days.length - 1])
             .toString();
 
-        return this.httpClient.post<Schedule>(url + params, postBody, { headers: this._headers })
+        return this.postRequest<Schedule>(url + params, postBody, this._headers)
             .pipe(map(res => res.schedule));
     }
 
@@ -84,20 +84,32 @@ export class ApiService {
         const scheduleDate = parse(selectedClass.open_registration, "yyyy-MM-dd HH:mm:ss", new Date());
         if (!isValid(scheduleDate)) throw new Error('Invalid class date');
 
-        const body = {
+        const runTaskBody = {
             url: `${BASE_URL}/class-booking.php?json&clubid=${selectedClubId}&id=${selectedClass.id}`,
+            method: 'POST',
             headers: {
                 Auth: `Bearer ${this._token}`,
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Sec-Fetch-Site': 'cross-site',
+                'Sec-Fetch-Mode': 'cors',
+                'Sec-Fetch-Dest': 'empty',
+                'Origin': 'capacitor://localhost',
+                'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
             },
-            body: btoa(postBody),
+            body: postBody
+        };
+
+        const addTaskBody = {
+            url: `${TASKS_URL}?type=RUN_TASK`,
+            body: btoa(JSON.stringify(runTaskBody)),
+            headers: { 'Content-Type': 'application/json' },
             scheduleTimestamp: getUnixTime(scheduleDate)
         };
 
         const params = new HttpParams()
             .set('type', 'ADD_TASK')
             .set('queue', this._queue);
-        return this.httpClient.post(TASKS_URL, body, { params });
+        return this.httpClient.post(TASKS_URL, addTaskBody, { params });
     }
 
     deleteBooking(taskId: string) {
@@ -106,5 +118,20 @@ export class ApiService {
             .set('queue', this._queue)
             .set('taskId', taskId);
         return this.httpClient.get(TASKS_URL, { params, responseType: 'text' });
+    }
+
+    postRequest<T>(url: string, postBody: string, headers: {[key: string]: string}) {
+        const runTaskBody = {
+            url,
+            method: 'POST',
+            headers,
+            body: postBody
+        };
+
+        return this.httpClient.post<T>(
+            `${TASKS_URL}?type=RUN_TASK`,
+            JSON.stringify(runTaskBody),
+            { headers: { 'Content-Type': 'application/json' } }
+        );
     }
 }
